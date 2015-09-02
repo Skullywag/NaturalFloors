107c573e0454bccb6cfff311ab0aa820ebb6a861
Skullywag <mjskull@gmail.com>
2015-09-02 17:46:36 +0100

    initial commit

---
+++ Source/NaturalFloors/Building_Composter.cs
@@ -0,0 +1,60 @@
+﻿using Verse;
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Verse.Sound;
+using RimWorld;
+namespace NaturalFloors
+{
+	public class Building_Composter : Building_Storage
+	{
+		public override void SpawnSetup()
+		{
+			base.SpawnSetup();
+		}
+
+        public override void TickRare()
+        {
+            //List all things in cell
+            List<Thing> list = Find.ThingGrid.ThingsListAt(Position);
+            if (list.Count > 0)
+            {
+                //Loop over things
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i].def.category == ThingCategory.Item)
+                    {
+                        if (list[i].def.defName != "FreshCompost")
+                        {
+                            DamageInfo dinfo = new DamageInfo(DamageDefOf.Rotting, 1, null, null, null);
+                            Thing compost = ThingMaker.MakeThing(ThingDef.Named("FreshCompost"));
+                            Thing thing = list[i];
+                            int stacksize = thing.stackCount;
+                            if (thing.def.defName.Contains("Corpse"))
+                            {
+                                Corpse corpse = thing as Corpse;
+                                if (corpse.innerPawn.def.race.Animal || corpse.innerPawn.def.race.Humanlike)
+                                {
+                                    thing.TakeDamage(dinfo);
+                                    compost.stackCount = 25;
+                                }      
+                            }
+                            else
+                            {
+                                thing.TakeDamage(dinfo);
+                                compost.stackCount = stacksize;
+                            }
+                            if (thing.HitPoints < 10)
+                            {
+                                thing.Destroy();
+                                GenSpawn.Spawn(compost, Position);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+	}
+}
---
+++ Source/NaturalFloors/PlaceWorker_NearWater.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Verse;
+using RimWorld;
+namespace NaturalFloors
+{
+    public class PlaceWorker_NearWater : PlaceWorker
+    {
+        public bool water = false;
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot)
+        {
+            foreach (IntVec3 current in GenRadial.RadialCellsAround(loc, 5f, true))
+            {
+                if(current.GetTerrain() == TerrainDef.Named("WaterDeep") || current.GetTerrain() == TerrainDef.Named("WaterShallow") || current.GetTerrain() == TerrainDef.Named("Marsh"))
+                {
+                    water = true;
+                }         
+            }
+            if(water == false)
+            {
+                return new AcceptanceReport("Must be near water source.");
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
